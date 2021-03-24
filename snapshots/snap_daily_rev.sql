b89597acfc26f341d6b6f21930d629c0603ada63
Mario Loera <marioll@kth.se>
2021-03-24 18:17:50 +0000

    snapshot strategy timestamp, using updated_at='loaded_at'

--- snapshots/snap_daily_rev.sql
+++ snapshots/snap_daily_rev.sql
@@ -1,7 +1,20 @@
+{% snapshot orders_snapshot %}
+
+{{
+    config(
+      target_database='mllkthdbt',
+      target_schema='dbt_snapshots',
+      unique_key='date',
+
+      strategy='timestamp',
+      updated_at='loaded_at',
+    )
+}}
 
 select
-    current_date,
-    current_timestamp as time,
+    current_date as date,
+    current_timestamp as loaded_at,
     sum(amount) as daily_rev
 from {{ ref('fct_orders') }}
 
+{% endsnapshot %}
